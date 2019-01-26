373f4985a881d39e8c81b66875bccb23ff60669b
usamajamil006 <usamajamil006@gmail.com>
2019-01-26 18:12:59 +0500

    Styling the Recipe compnent

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -12,17 +12,30 @@ class Recipe extends Component {
 
     componentDidMount = async () => {
         const title = this.props.location.state.recipe
-
+        console.log(title)
         const searchRequest = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${title}`);
 
         const res = await searchRequest.json();
+        console.log(res)
         this.setState({ activeRecipe: res.recipes[0] })
     }
     render() {
-        return (<div>
 
-
-        </div>)
+        const recipe = this.state.activeRecipe
+        return (
+            <div className="container">
+                <div className="active-recipe">
+                    <img className="active-recipe__img" src={recipe.image_url} alt={recipe.title} />
+                    <h3 className="active--recipe__title">{recipe.title}</h3>
+                    <h4 className="active--recipe__publisher">Publisher:
+                <span>{recipe.publisher}</span>
+                    </h4>
+                    <p className="active-recipe__website">Website:
+                <span><a href={recipe.publisher_url}>{recipe.publisher_url}</a></span>
+                    </p>
+                    <button className="active-recipe__button">Go Home</button>
+                </div>
+            </div>)
     }
 }
 export default Recipe
