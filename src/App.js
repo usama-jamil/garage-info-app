6789a7114fca82ad7f4c7f201184d998eec66084
usamajamil006 <usamajamil006@gmail.com>
2019-01-26 14:10:39 +0500

    Get a Api Call

--- src/App.js
+++ src/App.js
@@ -2,31 +2,31 @@ import React, { Component } from "react";
 import "./App.css";
 import Form from "./components/Form";
 
-const API_KEY = "fkjsdhfkjshf";
+const API_KEY = "a05665efd3aff1fb699c986be7b0cc12";
 class App extends Component {
 
 state = {
   recipes: []
 }
 
-  getRecipes = async e => {
+  getRecipe = async e => {
 
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
   
-    const api_call = await fetch(`dlkfjklsj${API_KEY}`);
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=10`);
     const data = await api_call.json();
 
-    console.log(recipeName);
+    console.log(data.recipes);
   };
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title"> Garage Search</h1>
+          <h1 className="App-title"> Recipe Search</h1>
         </header>
-        <Form getGarages={this.getGarages} />
+        <Form getRecipe={this.getRecipe} />
       </div>
     );
   }
