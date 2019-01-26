5dc47c01a0ee1bbc6ff6a2c68703780d23dd252c
usamajamil006 <usamajamil006@gmail.com>
2019-01-26 14:26:18 +0500

    Make search functional

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import Form from "./components/Form";
 const API_KEY = "a05665efd3aff1fb699c986be7b0cc12";
 class App extends Component {
 
-state = {
-  recipes: []
-}
+  state = {
+    recipes: []
+  }
 
   getRecipe = async e => {
 
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-  
-    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=10`);
+
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
     const data = await api_call.json();
 
-    console.log(data.recipes);
+    this.setState({ recipes: data.recipes })
   };
 
   render() {
@@ -27,6 +27,11 @@ state = {
           <h1 className="App-title"> Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe} />
+        {this.state.recipes.map(recipe =>
+          (
+            <p key={recipe.recipe_id}>{recipe.title}</p>
+          )
+        )}
       </div>
     );
   }
