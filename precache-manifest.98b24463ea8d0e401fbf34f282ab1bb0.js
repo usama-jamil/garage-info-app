6d39426426195840b32d99f016913d96ac0bcf73
usamajamil006 <usamajamil006@gmail.com>
2019-01-30 15:26:26 +0500

    Updates

--- precache-manifest.98b24463ea8d0e401fbf34f282ab1bb0.js
+++ precache-manifest.98b24463ea8d0e401fbf34f282ab1bb0.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/garage-info-app/static/js/runtime~main.f0167697.js"
   },
   {
-    "revision": "97308b11e0d7cf66ea5a",
-    "url": "/garage-info-app/static/js/main.97308b11.chunk.js"
+    "revision": "54583d0ad6cb6d8f515d",
+    "url": "/garage-info-app/static/js/main.54583d0a.chunk.js"
   },
   {
     "revision": "b4ba9e8f3fe84f3162af",
     "url": "/garage-info-app/static/js/1.b4ba9e8f.chunk.js"
   },
   {
-    "revision": "97308b11e0d7cf66ea5a",
+    "revision": "54583d0ad6cb6d8f515d",
     "url": "/garage-info-app/static/css/main.c1d50506.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/garage-info-app/static/css/1.a3821e50.chunk.css"
   },
   {
-    "revision": "1ea1a8b0f4cd3cf49b093c73e5781447",
+    "revision": "872a6a1eec34728bf2bc98cfbc840064",
     "url": "/garage-info-app/index.html"
   }
 ];
