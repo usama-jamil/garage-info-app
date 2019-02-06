e87d78da752c68b5f2e107532b82ca4cc9c8e966
usamajamil006 <usamajamil006@gmail.com>
2019-02-06 10:32:16 +0500

    Add routes fo  that user Page

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,18 +3,34 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import App from "../App";
 import Recipe from "./Recipe";
-import Login from "./Login";
-import SignUp from "./SignUp";
+import Login from "./Auth/Login";
+import SignUp from "./Auth/SignUp";
+import PublicGaragePage from "./PublicGaragePage";
+import UserGaragePage from "./UserGaragePage";
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" component={App} exact />
       <Route path="/recipe/:id" component={Recipe} />
+                                    {/*give the property for login user  */}
+      <GaragePageRoute path="/page/:id" loginUser={"loginuser"} />
       <Route path="/login" component={Login} />
       <Route path="/signup" component={SignUp} />
     </Switch>
   </BrowserRouter>
 );
 
+const GaragePageRoute = ({ path, loginUser, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      loginUser === "loginuser" ? ( // Make logic for login user  access their own page  it get more detail then public user
+        <UserGaragePage {...props} />
+      ) : (
+        <PublicGaragePage {...props} />
+      )
+    }
+  />
+);
 export default Router;
