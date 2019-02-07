2696761830127f338ebc3ef447dbbccf7463852e
usamajamil006 <usamajamil006@gmail.com>
2019-02-07 15:47:58 +0500

    show header in all pages

--- src/components/Router.js
+++ src/components/Router.js
@@ -6,20 +6,23 @@ import Recipe from "./Recipe";
 import Login from "./Auth/Login";
 import SignUp from "./Auth/SignUp";
 
-
 import PublicGaragePage from "../container/garagePage/PublicGaragePage";
 import UserGaragePage from "../container/garagePage/UserGaragePage";
+import Header from "../components/Header";
 
 const Router = () => (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={App} exact />
-      <Route path="/recipe/:id" component={Recipe} />
-                                    {/*give the property for login user  */}
-      <GaragePageRoute path="/page/:id" loginUser={"loginuser"} />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={SignUp} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={App} exact />
+        <Route path="/recipe/:id" component={Recipe} />
+        {/*give the property for login user  */}
+        <GaragePageRoute path="/page/:id" loginUser={"loginuser"} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 
@@ -27,7 +30,7 @@ const GaragePageRoute = ({ path, loginUser, ...rest }) => (
   <Route
     {...rest}
     render={props =>
-      loginUser === "loginuser" ? ( // Make logic for login user  access their own page  it get more detail then public user
+      loginUser === "loginur" ? ( // Make logic for login user  access their own page  it get more detail then public user
         <UserGaragePage {...props} />
       ) : (
         <PublicGaragePage {...props} />
