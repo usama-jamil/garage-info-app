9b5d679812bb2dfff0c11edf93615f92671d5b4b
usamajamil006 <usamajamil006@gmail.com>
2019-02-07 17:15:06 +0500

    use provider  to bind react to redux

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import Router from './components/Router'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import Router from "./components/Router";
 
-import { createStore } from 'redux';
+import { createStore } from "redux";
 //Simply importing config file of firebase
-import './config'
+import "./config";
 //that  object  hold all the  reducer in the app
-import rootReducer from './store/reducers/rootReducer';
-
-
+import rootReducer from "./store/reducers/rootReducer";
+//Provider is use for bind the store to react
+import { Provider } from "react-redux";
 
 //here  you pass object of reducer that manage by createStore
-const store= createStore(rootReducer)
-ReactDOM.render(<Router />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
