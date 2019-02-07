db66e959b103916e84674947c8451c774dd0c4c3
usamajamil006 <usamajamil006@gmail.com>
2019-02-07 16:00:25 +0500

    To make is dunamic we use id to get data from  database

--- src/container/garagePage/PublicGaragePage.js
+++ src/container/garagePage/PublicGaragePage.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 
-const PublicGaragePage = () => "PublicGaragePage";
+const PublicGaragePage = (props) => {
+    //we  get id from the route  and use that  id to  extract data from database
+    const id = props.match.prams.id
+};
 
 export default PublicGaragePage;
--- src/container/garagePage/UserGaragePage.js
+++ src/container/garagePage/UserGaragePage.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
-const UserGaragePage = () => "<div />UserGaragePage"
+const UserGaragePage = (props) => {
+    //we  get id from the route  and use that  id to  extract data from database
+    const id = props.match.prams.id
+    
+}
 
 
 export default UserGaragePage;
